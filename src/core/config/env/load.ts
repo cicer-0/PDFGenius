41e1fc9994d9b55908d7fef11e57b894480f6926
cicer <git.use@outlook.com>
2024-02-25 20:29:10 -0500

    git commit -m "Refactorización: Actualizar comandos de inicio en package.json para simplificar y mejorar la legibilidad.
    En el archivo package.json, se han modificado los comandos 'start' y 'start:dev' para utilizar simplemente 'node' y 'nodemon' respectivamente, en lugar de establecer explícitamente las variables de entorno NODE_ENV. Esto simplifica la configuración y hace que sea más claro cómo iniciar la aplicación en producción y desarrollo.
    
    Además, se ha corregido la ruta de los archivos de configuración de entorno en load.ts para que coincida con la ubicación real del archivo .env.
    
    Estas modificaciones mejoran la legibilidad del código y facilitan el mantenimiento futuro de la aplicación."

--- src/core/config/env/load.ts
+++ src/core/config/env/load.ts
@@ -1,25 +1,12 @@
 import dotenv from 'dotenv';
 import path from 'path';
-import { Environment, validEnvironments } from './types';
 import { validator } from './validator';
 
-const ENV_FILE_BASE_PATH = '../../../.env.';
-
-function validateNodeEnv(nodeEnv: string | undefined) {
-	if (!validEnvironments.includes(nodeEnv as Environment)) {
-		throw new Error(
-			`Error: Invalid NODE_ENV value. NODE_ENV must be one of ${validEnvironments.join(
-				', '
-			)}.`
-		);
-	}
-}
+const ENV_FILE_BASE_PATH = '../../../.env';
 
 export function load() {
-	const nodeEnv = process.env.NODE_ENV;
-	validateNodeEnv(nodeEnv);
 	dotenv.config({
-		path: path.resolve(__dirname, `${ENV_FILE_BASE_PATH}${nodeEnv}`),
+		path: path.resolve(__dirname, `${ENV_FILE_BASE_PATH}`),
 	});
 	validator();
 }
